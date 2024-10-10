b034b0dd4fa1755d475d2d791e34fe2dbfb74e6c
ilya.ignatev <ilya.ignatev@onlinesup.com>
2024-10-10 12:33:32 +0300

    Pipipi

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -20,18 +20,16 @@ function generateRandomNickname(length = 8): string {
 }
 
 let socket: Socket;
-let peerConnection: RTCPeerConnection;
-let dataChannel: RTCDataChannel;
 
-const socketAvailable  = ref<boolean>(false);
-const peerAvailable    = ref<boolean>(false);
-const messageAvailable = ref<boolean>(false);
+const peerConnection        = ref<RTCPeerConnection>();
+const peerConnectionLoading = ref<boolean>(false);
+
+const socketAvailable = ref<boolean>(false);
+const peerAvailable   = ref<boolean>(false);
 
 const users        = ref<string[]>([]);
 const selectedUser = ref<string>();
 const myUser       = ref<string>();
-const message      = ref<string>('');
-const messages     = ref<any[]>([]);
 
 const inputRef  = ref();
 const scrollRef = ref();
@@ -42,31 +40,69 @@ const mineAudioRef = ref();
 const friendVideoRef = ref();
 const friendAudioRef = ref();
 
-const selectedVideo = ref();
-const selectedAudio = ref();
+const selectedVideo           = ref();
+const selectedVideoResolution = ref();
+const selectedVideoFrameRate  = ref();
+const selectedAudio           = ref();
 
-const selectedVideoDeviceId = computed(() => selectedVideo.value ? {deviceId: selectedVideo.value?.deviceId} : false);
-const selectedAudioDeviceId = computed(() => selectedAudio.value ? {deviceId: selectedAudio.value?.deviceId} : false);
+const videoResolutions = computed(() => {
+    if (!selectedVideo.value)
+        return [];
 
-const {videoInputs: cameras, audioInputs: microphones} = useDevicesList({requestPermissions: true});
+    const cpb = selectedVideo.value.getCapabilities();
+
+    return [{
+        width : cpb.width.max,
+        height: cpb.height.max,
+        label : cpb.width.max + ' x ' + cpb.height.max
+    }]
+});
+
+const videoFrameRates = computed(() => {
+    if (!selectedVideo.value)
+        return [];
+
+    const cpb = selectedVideo.value.getCapabilities();
+
+    return [{
+        value: cpb.frameRate.max,
+        label: cpb.frameRate.max.toFixed(2) + ' FPS'
+    }]
+});
+
+watch(videoResolutions, value => {
+    selectedVideoResolution.value = value[0] ?? undefined;
+});
+
+watch(videoFrameRates, value => {
+    selectedVideoFrameRate.value = value[0] ?? undefined;
+});
+
+const statistics = ref([]);
+
+const selectedVideoDevice = computed(() => selectedVideo.value ? {
+    deviceId : selectedVideo.value?.deviceId,
+    width    : {ideal: selectedVideoResolution.value.width},
+    height   : {ideal: selectedVideoResolution.value.height},
+    frameRate: {ideal: selectedVideoFrameRate.value.value}
+
+} : false);
+const selectedAudioDevice = computed(() => selectedAudio.value ? {deviceId: selectedAudio.value?.deviceId} : false);
+
+const {videoInputs: cameras, audioInputs: microphones, devices} = useDevicesList({requestPermissions: true});
+
+watch(cameras, value => {
+    value.forEach(camera => console.log(camera.label, camera.getCapabilities()));
+}, {immediate: true});
 
 const {stream, start} = useUserMedia({
     constraints: {
-        video: selectedVideoDeviceId,
-        audio: selectedAudioDeviceId
+        video: selectedVideoDevice,
+        audio: selectedAudioDevice
     }
 });
 
-async function connectToUser(user: string) {
-    selectedUser.value = user;
-
-    if (peerConnection)
-        peerConnection.close();
-
-    peerConnection = makePeerConnection();
-
-    dataChannel = makeDataChannel();
-
+async function addStreamTo(connection: RTCPeerConnection) {
     if (selectedVideo.value || selectedAudio.value) {
         await start();
 
@@ -75,7 +111,16 @@ async function connectToUser(user: string) {
                 if (!stream.value)
                     return;
 
-                peerConnection.addTrack(track, stream.value);
+                connection.addTrack(track, stream.value);
+
+                if (track.kind == 'audio') {
+                    const mediaStream = new MediaStream();
+                    mediaStream.addTrack(track);
+
+                    mineAudioRef.value.srcObject = mediaStream;
+                    mineAudioRef.value.volume    = 0;
+                    mineAudioRef.value.play();
+                }
 
                 if (track.kind == 'video') {
                     const mediaStream = new MediaStream();
@@ -87,21 +132,38 @@ async function connectToUser(user: string) {
             });
         }
     }
-
-    const offer = await peerConnection.createOffer();
-    await peerConnection.setLocalDescription(offer);
-
-    socket.emit('offer', offer, user);
 }
 
-function makePeerConnection(): RTCPeerConnection {
+async function connectToUser(user: string) {
+    if (!peerConnection.value)
+        peerConnection.value = await makePeerConnection();
+
+    selectedUser.value = user;
+
+    await addStreamTo(peerConnection.value);
+
+    const offer = await peerConnection.value.createOffer();
+    console.log('Make offer', offer);
+    await peerConnection.value.setLocalDescription(offer);
+    console.log('Set local description to offer');
+
+    socket.emit('offer', offer, user);
+    console.log('Emit offer to', user);
+}
+
+async function makePeerConnection(): Promise<RTCPeerConnection> {
+    peerConnectionLoading.value = true;
+
     const connection = new RTCPeerConnection({
         iceServers: [{urls: 'stun:stun.l.google.com:19302'}]
     });
 
+    console.log('Peer connection constructed');
+
     connection.onicecandidate = (event: RTCPeerConnectionIceEvent) => {
         if (event.candidate) {
             socket.emit('candidate', event.candidate, selectedUser.value);
+            console.log('Emit candidate', event.candidate);
         }
     };
 
@@ -130,89 +192,26 @@ function makePeerConnection(): RTCPeerConnection {
         peerAvailable.value = connection.connectionState == "connected";
     };
 
+    peerConnectionLoading.value = false;
+
     return connection;
 }
 
-function makeDataChannel() {
-    const dataChannel = peerConnection.createDataChannel('channelName');
+watch(peerConnection, connection => {
+    setInterval(() => {
+        if (connection?.connectionState != 'connected')
+            return;
 
-    dataChannel.onmessage = (event: MessageEvent<any>) => {
-        console.log('Message from data channel', event.data);
-        messageHandler(event.data);
-    };
-
-    dataChannel.onopen = () => {
-        console.log('Data channel is open');
-        messageAvailable.value = true;
-    };
-
-    dataChannel.onclose = () => {
-        console.log('Data channel is closed');
-        messageAvailable.value = false;
-    };
-
-    return dataChannel;
-}
-
-function sendMessage() {
-    dataChannel.send(message.value);
-    messages.value.push({
-        from   : 'me',
-        content: message.value,
-        type   : 'text'
-    });
-    message.value = '';
-}
-
-function sendFile(file: File | null) {
-    if (!file)
-        return;
-
-    inputRef.value.value = null;
-
-    dataChannel.send(JSON.stringify({type: file.type, forNext: true}));
-    dataChannel.send(file);
-
-    messages.value.push({
-        from   : 'me',
-        content: file,
-        type   : 'file'
-    });
-}
-
-function downloadFile(file: Blob) {
-    navigateTo(URL.createObjectURL(file), {open: {target: "_blank"}});
-}
-
-let forNextData: any = null;
-
-function messageHandler(data: any) {
-    try {
-        const _data = JSON.parse(data);
-
-        if (_data.forNext)
-            forNextData = _data;
-
-        return;
-    } catch {
-        messages.value.push({
-            from   : selectedUser.value,
-            content: data instanceof ArrayBuffer && forNextData ? new Blob([data], {type: forNextData.type}) : data,
-            type   : data instanceof ArrayBuffer ? 'file' : 'text'
+        connection.getStats(null).then(stats => {
+            statistics.value = [...stats];
         });
-
-        forNextData = null;
-    }
-}
-
-watch(() => messages.value.length, value => {
-    setTimeout(() => scrollRef.value.scrollTop = scrollRef.value.scrollHeight, 200);
+    }, 1000);
 });
 
 onMounted(() => {
     if (socket) {
-        if (peerConnection)
-            peerConnection.close();
+        if (peerConnection.value)
+            peerConnection.value.close();
 
         if (selectedUser.value)
             selectedUser.value = undefined;
@@ -230,9 +229,12 @@ onMounted(() => {
         const username = generateRandomNickname();
         socket.emit('register', username);
         myUser.value = username;
+
+        console.log('Connect as', username);
     });
 
     socket.on('disconnect', () => {
+        console.log('Disconnected');
         socketAvailable.value = false;
     });
 
@@ -241,70 +243,48 @@ onMounted(() => {
     });
 
     socket.on('offer', async (offer: RTCSessionDescriptionInit, from: string) => {
-        if (peerConnection)
-            peerConnection.close();
+        if (!peerConnection.value)
+            peerConnection.value = await makePeerConnection();
 
-        peerConnection = makePeerConnection();
+        console.log('Catch offer from', from);
 
-        peerConnection.ondatachannel = (event: RTCDataChannelEvent) => {
-            dataChannel = event.channel;
+        await peerConnection.value.setRemoteDescription(offer);
+        console.log('Set remote description', offer);
 
-            dataChannel.onmessage = (event: MessageEvent<any>) => {
-                console.log('Message from data channel', event.data);
-                messageHandler(event.data);
-            };
+        await addStreamTo(peerConnection.value);
 
-            dataChannel.onopen = () => {
-                console.log('Data channel is open');
-                messageAvailable.value = true;
-            };
+        const answer = await peerConnection.value.createAnswer();
+        console.log('Make answer', answer);
 
-            dataChannel.onclose = () => {
-                console.log('Data channel is closed');
-                messageAvailable.value = false;
-            };
-        };
-
-        await peerConnection.setRemoteDescription(offer);
-
-        if (selectedVideo.value || selectedAudio.value) {
-            await start();
-
-            if (stream.value) {
-                stream.value.getTracks().forEach(track => {
-                    if (!stream.value)
-                        return;
-
-                    peerConnection.addTrack(track, stream.value);
-
-                    if (track.kind == 'video') {
-                        const mediaStream = new MediaStream();
-                        mediaStream.addTrack(track);
-
-                        mineVideoRef.value.srcObject = mediaStream;
-                        mineVideoRef.value.play();
-                    }
-                });
-            }
-        }
-
-        const answer = await peerConnection.createAnswer();
-        await peerConnection.setLocalDescription(answer);
+        await peerConnection.value.setLocalDescription(answer);
+        console.log('Set local description to answer');
 
         socket.emit('answer', answer, from);
+        console.log('Emit answer to', from);
 
         selectedUser.value = from;
     });
 
     socket.on('answer', async (answer: RTCSessionDescriptionInit) => {
-        await peerConnection.setRemoteDescription(answer);
+        console.log('Catch answer', answer);
+        await peerConnection.value.setRemoteDescription(answer);
+        console.log('Set remote description to answer');
+
+        socket.emit('ready', selectedUser.value);
+        console.log('Emit ready signal to', selectedUser.value);
     });
 
     socket.on('candidate', (candidate: RTCIceCandidate) => {
-        peerConnection.addIceCandidate(new RTCIceCandidate(candidate))
+        console.log('Catch candidate', candidate);
+        peerConnection.value.addIceCandidate(new RTCIceCandidate(candidate))
                       .catch(error => {
                           console.error('Error adding received ICE candidate', error);
                       });
+        console.log('Add ice candidate');
+    });
+
+    socket.on('ready', async (from: string) => {
+        console.log('Catch ready signal from', from);
     });
 });
 
@@ -316,76 +296,28 @@ onBeforeUnmount(() => {
 
 <template>
     <UContainer class="py-5">
-        <div class="flex gap-5">
-            <UCard class="grow">
-                <template #header>
-                    <h5 class="font-semibold">Users</h5>
-                </template>
+        <UCard class="grow">
+            <template #header>
+                <h5 class="font-semibold">Users</h5>
+            </template>
 
-                <div class="flex flex-col gap-2.5">
-                    <UButton v-for="user in users"
-                             :label="myUser == user ? 'Me' : user"
-                             icon="i-heroicons-user"
-                             :variant="user == selectedUser ? 'solid' : 'soft'"
-                             color="gray"
-                             :disabled="user == selectedUser || user == myUser"
-                             @click="connectToUser(user)"/>
-                </div>
-            </UCard>
-
-            <UCard class="flex flex-col grow h-[300px]" :ui="{body: {base: 'grow h-0', padding: ''}}">
-                <template #header>
-                    <h5 class="font-semibold">Data channel</h5>
-                </template>
-
-                <div ref="scrollRef" class="flex flex-col gap-2 grow h-full overflow-auto px-6 py-4">
-                    <div v-for="message in messages">
-                        <span class="font-semibold">{{ message.from }}:</span>
-
-                        <span v-if="message.type == 'text'" class="ms-1.5">
-                        {{ message.content }}
-                    </span>
-
-                        <div v-else class="inline-flex items-center gap-1.5 ms-1.5 border rounded-md px-1.5 py-1">
-                            <span class="text-sm">File</span>
-                            <UButton size="xs"
-                                     icon="i-heroicons-arrow-down-tray"
-                                     color="gray"
-                                     @click="downloadFile(message.content)"/>
-                        </div>
-                    </div>
-                </div>
-
-                <template #footer>
-                    <div class="flex gap-2.5">
-                        <input ref="inputRef"
-                               type="file"
-                               class="hidden"
-                               @change="sendFile($event.target.files[0] ?? null)"/>
-
-                        <UButton color="gray"
-                                 icon="i-heroicons-paper-clip-solid"
-                                 :disabled="!messageAvailable"
-                                 @click="inputRef.click()"/>
-
-                        <UInput placeholder="Enter message..."
-                                class="grow"
-                                @keydown.enter="sendMessage"
-                                v-model="message"/>
-
-                        <UButton label="Send"
-                                 color="gray"
-                                 :disabled="message.trim().length == 0 || !messageAvailable"
-                                 @click="sendMessage"/>
-                    </div>
-                </template>
-            </UCard>
-        </div>
+            <div class="flex flex-col gap-2.5">
+                <UButton v-for="user in users"
+                         :label="myUser == user ? 'Me' : user"
+                         icon="i-heroicons-user"
+                         :variant="user == selectedUser ? 'solid' : 'soft'"
+                         color="gray"
+                         :disabled="user == selectedUser || user == myUser"
+                         @click="connectToUser(user)"/>
+            </div>
+        </UCard>
 
         <div class="mt-5 flex gap-5">
             <UCard class="flex flex-col grow w-1/2">
                 <template #header>
-                    <h5 class="font-semibold">Me</h5>
+                    <div class="flex justify-between items-center">
+                        <h5 class="font-semibold">Me</h5>
+                    </div>
                 </template>
 
                 <div class="flex flex-col gap-2.5">
@@ -393,11 +325,25 @@ onBeforeUnmount(() => {
                     <audio ref="mineAudioRef" autoplay controls/>
 
                     <div class="flex gap-2.5">
-                        <UFormGroup label="Video" class="w-1/2">
-                            <USelectMenu placeholder="Select video device"
-                                         :options="cameras"
-                                         v-model="selectedVideo"/>
-                        </UFormGroup>
+                        <div class="flex flex-col gap-2.5 w-1/2">
+                            <UFormGroup label="Video">
+                                <USelectMenu placeholder="Select video device"
+                                             :options="cameras"
+                                             v-model="selectedVideo"/>
+                            </UFormGroup>
+
+                            <UFormGroup v-if="selectedVideo" label="Resolution">
+                                <USelectMenu placeholder="Select video device"
+                                             :options="videoResolutions"
+                                             v-model="selectedVideoResolution"/>
+                            </UFormGroup>
+
+                            <UFormGroup v-if="selectedVideo" label="Frame rate">
+                                <USelectMenu placeholder="Select frame rate"
+                                             :options="videoFrameRates"
+                                             v-model="selectedVideoFrameRate"/>
+                            </UFormGroup>
+                        </div>
 
                         <UFormGroup label="Audio" class="w-1/2">
                             <USelectMenu placeholder="Select audio device"
@@ -420,8 +366,8 @@ onBeforeUnmount(() => {
             </UCard>
         </div>
 
-        <div class="mt-5">
-            <UCard>
+        <div class="mt-5 flex gap-5">
+            <UCard class="flex flex-col grow w-1/2">
                 <template #header>
                     <h5 class="font-semibold">Status</h5>
                 </template>
@@ -429,7 +375,24 @@ onBeforeUnmount(() => {
                 <div class="flex flex-col gap-2.5">
                     <SignalLabel :active="socketAvailable" label="Socket connection"/>
                     <SignalLabel :active="peerAvailable" label="Peer connection"/>
-                    <SignalLabel :active="messageAvailable" label="Data channel connection"/>
+                </div>
+            </UCard>
+
+            <UCard class="flex flex-col grow w-1/2" :ui="{body: {padding: ''}}">
+                <template #header>
+                    <h5 class="font-semibold">Statistics</h5>
+                </template>
+
+                <div class="overflow-auto h-[400px] p-6 flex flex-col gap-5">
+                    <div
+                        v-for="stat in statistics.filter(_stat => ['inbound-rtp', 'outbound-rtp'].includes(_stat[1].type))">
+                        <p class="font-semibold text-xl">{{ stat[1].type }}</p>
+                        <p>Kind: {{ stat[1].kind }}</p>
+                        <p>Codec ID: {{ stat[1].codecId }}</p>
+                        <p>Packets received: {{ stat[1].packetsReceived }} / {{ stat[1].packetsLost }} /
+                            {{ stat[1].packetsDiscarded }}</p>
+                        <p>Bytes received: {{ stat[1].bytesReceived }}</p>
+                    </div>
                 </div>
             </UCard>
         </div>
